cc297d1058047f39914abb562fe0137715cb0162
afistapratama12 <pratama.otori.12@gmail.com>
2025-04-09 09:37:28 +0700

    feat: add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="bottom-0">
+      <div className="flex justify-center">
+        <p className="text-gray-500 text-sm md:text-md">Made with ❤️ by
+          <span>
+            <a 
+            className="cursor-pointer hover:text-gray-400"
+            href="https://github.com/afistapratama12"
+            target="_blank"
+            rel="noopener noreferrer"
+            > afistapratama12
+            </a>
+          </span>
+        </p>
+      </div>
+    </footer>
+  )
+}
